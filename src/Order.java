0d6dc5b716ad7d150a9b4b0758eb6d75ee097fad
LeszekS <leszekstepien@o2.pl>
2019-02-26 14:36:53 +0100

    ver.11

---
+++ src/Order.java
@@ -0,0 +1,55 @@
+public class Order {
+    int id;
+    String name;
+    double price;
+    String status;
+
+    public Order(int id, String name, double price, String status) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
